40656eaf3a77d92d02ff7f6e903848da049fa44d
FabioVetricini <66067375+FabioVetricini@users.noreply.github.com>
2020-06-03 23:54:33 +0200

    Add files via upload

---
+++ Data/Module.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestionaleFitstic.Data
+{
+    public class Module
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Subject { get; set; }
+        public Course Course { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public Staff Teacher { get; set; }
+        public Staff Tutor { get; set; }
+        public Module()
+        { }
+    }
+}
---
+++ Data/Registration.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestionaleFitstic.Data
+{
+    public class Registration
+    {
+        [Key]
+        public int Id { get; set; }
+        public DateTime RegDate { get; set; }
+        public Course Course { get; set; }
+        public Student student { get; set; }        
+        public Registration()
+        { }
+
+    }
+}

1210c6c9d0b14252381989c006d68eda13cb6fe5
umbertoschiavone <schiavoneumberto00@gmail.com>
2020-06-11 19:53:47 +0200

    Modifiche pagine

--- GestionaleFitstic/GestionaleFitstic/Startup.cs
+++ GestionaleFitstic/GestionaleFitstic/Startup.cs
@@ -43,8 +43,10 @@ namespace GestionaleFitstic
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ApplicationDbContext db)
         {
+            db.Database.EnsureCreated();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();

42eb94382c8a361a2192bc6802b0b13e082dcf1c
FabioV88 <vetricini.fabio@gmail.com>
2020-06-23 13:38:48 +0200

    barra di ricerca + varie

---
+++ GestionaleFitstic/GestionaleFitstic/wwwroot/js/functions.js
@@ -0,0 +1,23 @@
+﻿function search() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("searchInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("dataTable");
+    tr = table.getElementsByTagName("tr");
+    thead = table.getElementsByTagName("thead")     
+    for (i = 0; i < tr.length; i++) {
+        for (c = 0; c < tr.length; c++) {
+            td = tr[i].getElementsByTagName("td")[c];
+            if (td) {
+                txtValue = td.textContent || td.innerText;
+                if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                    tr[i].style.display = "";
+                    break;
+                }
+                else {
+                    tr[i].style.display = "none";
+                }
+            }
+        }
+    }
+}
